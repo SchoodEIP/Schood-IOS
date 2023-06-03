51610ec88b85ef83ddc6fea7c2bd02aac26cc729
Mateo Deroche <mateo.deroche@epitech.eu>
2023-06-04 01:34:50 +0200

    [~] Remove useless file

--- Schood/Schood/View/HelpNumbers/HelpNumberSubCategoriesView.swift
+++ Schood/Schood/View/HelpNumbers/HelpNumberSubCategoriesView.swift
@@ -8,31 +8,10 @@
 import SwiftUI
 
 struct HelpNumberSubCategoriesView: View {
-    private var number: [NumberList] = NumberList.allNumber
     
     var body: some View {
         NavigationView {
-            List {
-                ForEach(number, id: \.name) { number in
-                    Section {
-                        NavigationLink(destination: HelpNumberDetailView(number: number)) {
-                            Text("\(number.name)")
-                                .font(.title2)
-                                .fontWeight(.medium)
-                        }
-                        .padding()
-                        .background(Color.customPurple)
-                    }
-                }
-                .cornerRadius(26)
-                .padding()
-                .foregroundColor(.customWrittingColorInvert)
-            }
-            .navigationBarItems(trailing: ProfileButton())
         }
-        .listStyle(.plain)
-        .background(Color.customWrittingColorInvert)
-        .navigationBarItems(trailing: ProfileButton())
     }
 }
 
